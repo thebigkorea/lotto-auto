e107e3336a2fc88b8781b2083c0cef31e5b31573
sangshin park <parkss9799@gmail.com>
2026-05-31 21:16:27 +0900

    과거 로또 데이터 기반 추천 기능 추가

---
+++ lotto-data.js
@@ -0,0 +1,33 @@
+const LOTTO_HISTORY = [
+
+  {
+    round: 1,
+    numbers: [10,23,29,33,37,40],
+    bonus: 16
+  },
+
+  {
+    round: 2,
+    numbers: [9,13,21,25,32,42],
+    bonus: 2
+  },
+
+  {
+    round: 3,
+    numbers: [11,16,19,21,27,31],
+    bonus: 30
+  },
+
+  {
+    round: 4,
+    numbers: [14,27,30,31,40,42],
+    bonus: 2
+  },
+
+  {
+    round: 5,
+    numbers: [16,24,29,40,41,42],
+    bonus: 3
+  }
+
+];
