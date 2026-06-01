5cc15097fe16e914071e5d96987e51b2f619a983
parkss9799 <parkss9799@gmail.com>
2026-06-01 13:05:46 +0900

    Use static lotto history

--- lotto-data.js
+++ lotto-data.js
@@ -1,33 +1,20 @@
 const LOTTO_HISTORY = [
-
   {
     round: 1,
+    date: "2002-12-07",
     numbers: [10,23,29,33,37,40],
     bonus: 16
   },
-
   {
     round: 2,
+    date: "2002-12-14",
     numbers: [9,13,21,25,32,42],
     bonus: 2
   },
-
   {
     round: 3,
+    date: "2002-12-21",
     numbers: [11,16,19,21,27,31],
     bonus: 30
-  },
-
-  {
-    round: 4,
-    numbers: [14,27,30,31,40,42],
-    bonus: 2
-  },
-
-  {
-    round: 5,
-    numbers: [16,24,29,40,41,42],
-    bonus: 3
   }
-
 ];
